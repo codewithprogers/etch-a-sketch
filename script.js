7598db73112a32d88a20ae888c5a7b2e26e68f7c
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-11-22 00:43:41 -0800

    feat: add hover drawing effect to grid tiles
    Implemented mouseenter and mouseleave events in JavaScript to allow
    drawing on the grid.

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ for(let i = 0; i < 16 * 16; i++) {
   square.classList.add("square");
   container.appendChild(square);
 }
+
+const gridTiles = document.querySelectorAll(".square");
+
+gridTiles.forEach(tile => {
+  tile.addEventListener("mouseenter", () => {
+    // tile.style.backgroundColor = "pink";
+    tile.classList.add("hovered");
+  });
+
+  tile.addEventListener("mouseleave", () => {
+    // tile.style.backgroundColor = "initial";
+    // tile.classList.remove("hovered");
+  })
+});
